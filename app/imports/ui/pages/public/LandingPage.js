c7aca086d84297d47ed1198a9967aa083fe0f586
philip nunoo <phanunoo@gmail.com>
2017-03-03 22:23:45 +0000

    little fixes

--- app/imports/ui/pages/public/LandingPage.js
+++ app/imports/ui/pages/public/LandingPage.js
@@ -10,29 +10,34 @@ import {
 import { LinkContainer } from 'react-router-bootstrap';
 
 const LandingPage = ({ children }) =>
-  <div>
-    <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/">App</a>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav pullRight>
-        <LinkContainer to="/sign-up" onlyActiveOnIndex>
-          <NavItem eventKey={1}>
-            Sign Up
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to="/sign-in" onlyActiveOnIndex>
-          <NavItem eventKey={1}>
-            Login
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar>
-    <Grid>
-      {children}
-    </Grid>
+  <div style={{ minHeight: '100%' }}>
+    <div className="footer-handler">
+      <Navbar fluid fixedTop className="navbar-public">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Mechanico</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav pullRight>
+          <LinkContainer to="/sign-up" onlyActiveOnIndex>
+            <NavItem eventKey={1}>
+              Sign Up
+            </NavItem>
+          </LinkContainer>
+          <LinkContainer to="/sign-in" onlyActiveOnIndex>
+            <NavItem eventKey={1}>
+              Login
+            </NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar>
+      <Grid style={{ paddingTop: 50 }}>
+        {children}
+      </Grid>
+    </div>
+    <footer className="footer">
+      Copyright &copy; 2017 Mechanico
+    </footer>
   </div>;
 
 LandingPage.propTypes = {
--- app/imports/ui/pages/public/LandingPageIndex.js
+++ app/imports/ui/pages/public/LandingPageIndex.js
@@ -6,9 +6,18 @@ import {
 } from 'react-bootstrap';
 
 const LandingPageIndex = () =>
-  <Grid>
+  <Grid style={{ minHeight: '100%' }}>
     <section>
-      Your Landing Page here.
+      <h1
+        style={{
+          textAlign: 'center',
+          padding: '15% 0px',
+          fontSize: 50,
+          fontWeight: 100,
+        }}
+      >
+        Your Landing Page here.
+      </h1>
     </section>
   </Grid>;
 
